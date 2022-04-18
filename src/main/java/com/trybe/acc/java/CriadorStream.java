d0598819780408ee0a066bd01138832fb900d2e2
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-18 11:43:00 -0300

    feat: item, array,lista

--- src/main/java/com/trybe/acc/java/CriadorStream.java
+++ src/main/java/com/trybe/acc/java/CriadorStream.java
@@ -1,5 +1,18 @@
 package com.trybe.acc.java;
 
-public class CriadorStream {
+import java.util.List;
+import java.util.stream.Stream;
 
+public class CriadorStream {
+  public <T> Stream<T> criarDoItem(T item) {
+    return Stream.of(item);
+  }
+
+  public <T> Stream<T> criarDoArray(T[] array) {
+    return Stream.of(array);
+  }
+
+  public <T> Stream<T> criarDaLista(List<T> lista) {
+    return lista.stream();
+  }
 }
